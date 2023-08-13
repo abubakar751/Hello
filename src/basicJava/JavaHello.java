bb357f9f23b5f67eb557cef3e58ad3d0629c833c
Abu Bakar <Abu Bakar@DESKTOP-H1FQGTF>
2023-08-13 12:39:34 +0530

    new commit

---
+++ src/basicJava/JavaHello.java
@@ -0,0 +1,13 @@
+package basicJava;
+
+public class JavaHello {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello world");
+		
+		}
+
+	}
+
+
---
+++ src/jdbc/JdbcDemo.java
@@ -0,0 +1,14 @@
+package jdbc;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class JdbcDemo {
+ BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+  
+         
+	 {
+
+		 }
+
+}
